62a4f58e0d051d92a4e969c318e17b5888a008b1
Eda <86181999+E-117@users.noreply.github.com>
2023-04-23 18:19:42 +0100

    Update quadratic-solver.py

--- quadratic-solver.py
+++ quadratic-solver.py
@@ -9,9 +9,5 @@ c = int(input('Give me the constant term: '))
 root1 = (-b + math.sqrt(b**2 - 4*a*c))/2*a
 root2 = (-b - math.sqrt(b**2 - 4*a*c))/2*a
 
-
-if root1 == root2:
-    print(f"The only solution is {root1}")
-else:
-    print("The only two solutions are:")
-    print(root1, "or", root2)
+print("The only two solutions are:")
+print(root1, "and", root2)
